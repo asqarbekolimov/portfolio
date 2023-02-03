8e85f415715b120ee89c9e2517ec3649067fc875
asqarbekolimov <mr.coderx.pro@gmail.com>
2023-02-04 00:21:59 +0500

    Burger menu

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,7 @@ import github from "./img/github.svg";
 import linkedin from "./img/linkedin.svg";
 import twitter from "./img/twitte.svg";
 import coder from "./img/coder.jfif";
+import menuBurger from "./img/burger.svg";
+import xIcon from "./img/xIcon.svg";
 
-export { Logo, github, linkedin, twitter, coder };
+export { Logo, github, linkedin, twitter, coder, menuBurger, xIcon };
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,21 @@
-import React from "react";
-import { github, linkedin, Logo, twitter } from "../assets";
+import React, { useState } from "react";
+import { github, linkedin, Logo, menuBurger, twitter, xIcon } from "../assets";
 
 function Navbar() {
+  const [toggle, setToggle] = useState(true);
+
+  const menuHandler = () => {
+    setToggle(!toggle);
+  };
+  console.log(toggle);
+
   return (
-    <header className="py-8 md:px-36 px-16">
+    <header className="py-8 md:px-36 px-6">
       <div className=" flex justify-between  items-center m-auto text-secondary">
         <img src={Logo} alt="" />
-        <ul className=" navbar md:flex  hidden gap-8 items-center">
+        <ul
+          className={` navbar active  md:flex hidden flex-col  gap-8 items-center `}
+        >
           <li>Home</li>
           <li>About</li>
           <li>Tech Stack</li>
@@ -18,9 +27,46 @@ function Navbar() {
             <img src={twitter} alt="" />
           </div>
         </ul>
+        <div className="menuBurger">
+          <img
+            className="w-9 md:hidden block cursor-pointer"
+            onClick={() => menuHandler()}
+            src={toggle ? menuBurger : xIcon}
+            alt=""
+          />
+        </div>
+        <div
+          className={`${
+            toggle ? "hidden" : "flex"
+          } p-12 absolute top-16 right-0 left-0  w-full`}
+        >
+          <ul
+            className={` py-10 navbarActive  md:flex  flex-col  gap-10 items-center `}
+          >
+            <li>Home</li>
+            <li>About</li>
+            <li>Tech Stack</li>
+            <li>Projects</li>
+            <li>Contact</li>
+            <div className=" flex gap-5 social-icon">
+              <a href="https://github.com/asqarbekolimov" target={"_blank"}>
+                <img src={github} alt="githubLogo" />
+              </a>
+              <a
+                href="https://uz.linkedin.com/in/asqarbekolimov"
+                target={"_blank"}
+              >
+                <img src={linkedin} alt="LinkedinLogo" />
+              </a>
+              <a href="https://twitter.com/asqarbek_olimov" target={"_blank"}>
+                <img src={twitter} alt="TwitterLogo" />
+              </a>
+            </div>
+          </ul>
+        </div>
       </div>
     </header>
   );
 }
-
+// https://uz.linkedin.com/in/asqarbekolimov
 export default Navbar;
