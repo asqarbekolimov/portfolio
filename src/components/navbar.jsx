d04bc790d21802a56184c3e5cd55ba4d9ae0304c
asqarbekolimov <mr.coderx.pro@gmail.com>
2023-02-04 00:41:35 +0500

    Responsive Burger menu

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,10 +11,10 @@ function Navbar() {
 
   return (
     <header className="py-8 md:px-36 px-6">
-      <div className=" flex justify-between  items-center m-auto text-secondary">
+      <div className=" flex justify-between items-center m-auto text-secondary">
         <img src={Logo} alt="" />
         <ul
-          className={` navbar active  md:flex hidden flex-col  gap-8 items-center `}
+          className={` navbar active  md:flex hidden flex-row  gap-8 items-center `}
         >
           <li>Home</li>
           <li>About</li>
@@ -22,12 +22,21 @@ function Navbar() {
           <li>Projects</li>
           <li>Contact</li>
           <div className=" flex gap-5 social-icon">
-            <img src={github} alt="" />
-            <img src={linkedin} alt="" />
-            <img src={twitter} alt="" />
+            <a href="https://github.com/asqarbekolimov" target={"_blank"}>
+              <img src={github} alt="githubLogo" />
+            </a>
+            <a
+              href="https://uz.linkedin.com/in/asqarbekolimov"
+              target={"_blank"}
+            >
+              <img src={linkedin} alt="LinkedinLogo" />
+            </a>
+            <a href="https://twitter.com/asqarbek_olimov" target={"_blank"}>
+              <img src={twitter} alt="TwitterLogo" />
+            </a>
           </div>
         </ul>
-        <div className="menuBurger">
+        <div className="menuBurger absolute right-12">
           <img
             className="w-9 md:hidden block cursor-pointer"
             onClick={() => menuHandler()}
@@ -35,35 +44,35 @@ function Navbar() {
             alt=""
           />
         </div>
-        <div
-          className={`${
-            toggle ? "hidden" : "flex"
-          } p-12 absolute top-16 right-0 left-0  w-full`}
+      </div>
+      <div
+        className={`${
+          toggle ? "hidden" : "flex"
+        } lg:hidden flex p-12 absolute top-16 right-0 left-0  w-full`}
+      >
+        <ul
+          className={` py-10 navbarActive  md:flex  flex-col  gap-10 items-center text-secondary`}
         >
-          <ul
-            className={` py-10 navbarActive  md:flex  flex-col  gap-10 items-center `}
-          >
-            <li>Home</li>
-            <li>About</li>
-            <li>Tech Stack</li>
-            <li>Projects</li>
-            <li>Contact</li>
-            <div className=" flex gap-5 social-icon">
-              <a href="https://github.com/asqarbekolimov" target={"_blank"}>
-                <img src={github} alt="githubLogo" />
-              </a>
-              <a
-                href="https://uz.linkedin.com/in/asqarbekolimov"
-                target={"_blank"}
-              >
-                <img src={linkedin} alt="LinkedinLogo" />
-              </a>
-              <a href="https://twitter.com/asqarbek_olimov" target={"_blank"}>
-                <img src={twitter} alt="TwitterLogo" />
-              </a>
-            </div>
-          </ul>
-        </div>
+          <li>Home</li>
+          <li>About</li>
+          <li>Tech Stack</li>
+          <li>Projects</li>
+          <li>Contact</li>
+          <div className=" flex gap-5 social-icon">
+            <a href="https://github.com/asqarbekolimov" target={"_blank"}>
+              <img src={github} alt="githubLogo" />
+            </a>
+            <a
+              href="https://uz.linkedin.com/in/asqarbekolimov"
+              target={"_blank"}
+            >
+              <img src={linkedin} alt="LinkedinLogo" />
+            </a>
+            <a href="https://twitter.com/asqarbek_olimov" target={"_blank"}>
+              <img src={twitter} alt="TwitterLogo" />
+            </a>
+          </div>
+        </ul>
       </div>
     </header>
   );
