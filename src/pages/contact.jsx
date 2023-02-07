5aa28b0ca56f8645c009156a88c251e8235a52f3
asqarbekolimov <mr.coderx.pro@gmail.com>
2023-02-07 22:16:46 +0500

    Contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -9,7 +9,57 @@ function Contact() {
       animate={{ width: "100%" }}
       exit={{ x: window.innerWidth, transition: { duration: 0.5 } }}
     >
-      Contact
+      <section>
+        <div className="flex justify-center items-center   dark:shadow-slate-900">
+          <div className="container mx-auto my-4 px-4 lg:px-20">
+            <div className="w-full p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 m-auto bg-slate-50 rounded-2xl shadow-2xl">
+              <form>
+                <div className="flex">
+                  <h1 className=" name font-bold uppercase text-5xl">
+                    Send us a <br /> message
+                  </h1>
+                </div>
+                <div className="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
+                  <input
+                    className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                    type="text"
+                    placeholder="First Name*"
+                  />
+                  <input
+                    className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                    type="text"
+                    placeholder="Last Name*"
+                  />
+                  <input
+                    className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                    type="email"
+                    placeholder="Email*"
+                  />
+                  <input
+                    className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                    type="number"
+                    placeholder="Phone*"
+                  />
+                </div>
+                <div className="my-4">
+                  <textarea
+                    placeholder="Message*"
+                    className="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                  ></textarea>
+                </div>
+                <div className="my-2 w-1/2 lg:w-1/4">
+                  <button
+                    className="name uppercase text-sm font-bold tracking-wide shadow-md text-gray-100 p-3 rounded-lg w-full 
+                      focus:outline-none focus:shadow-outline"
+                  >
+                    Send Message
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </motion.div>
   );
 }
