e6369b661e9cbe430b5454d771f668bc0de28c58
asqarbekolimov <mr.coderx.pro@gmail.com>
2023-02-12 14:33:52 +0500

    Form real time send msg

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import { motion } from "framer-motion";
 
 function Contact() {
+  const form = useRef();
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [msg, setMsg] = useState();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setName("");
+    setMsg("");
+    setEmail("");
+    emailjs
+      .sendForm(
+        "service_luzr4en",
+        "template_wu4i3w8",
+        form.current,
+        "hC7cFCja4I2WZEnaT"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("Successful send message");
+        },
+        (error) => {
+          console.log(error.text);
+          alert(error.text);
+        }
+      );
+  };
   return (
     <motion.div
       className=""
@@ -13,7 +42,7 @@ function Contact() {
         <div className="flex justify-center items-center   dark:shadow-slate-900">
           <div className="container mx-auto my-4 px-4 lg:px-20">
             <div className="w-full p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 m-auto bg-slate-50 rounded-2xl shadow-2xl">
-              <form>
+              <form ref={form} onSubmit={sendEmail}>
                 <div className="flex">
                   <h1 className=" name font-bold uppercase text-5xl">
                     Send us a <br /> message
@@ -23,26 +52,25 @@ function Contact() {
                   <input
                     className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                     type="text"
+                    name="user_name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
                     placeholder="First Name*"
                   />
                   <input
-                    className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                    type="text"
-                    placeholder="Last Name*"
-                  />
-                  <input
+                    name="user_email"
                     className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                     type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     placeholder="Email*"
-                  />
-                  <input
-                    className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                    type="number"
-                    placeholder="Phone*"
                   />
                 </div>
                 <div className="my-4">
                   <textarea
+                    name="message"
+                    value={msg}
+                    onChange={(e) => setMsg(e.target.value)}
                     placeholder="Message*"
                     className="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                   ></textarea>
@@ -51,6 +79,8 @@ function Contact() {
                   <button
                     className="name uppercase text-sm font-bold tracking-wide shadow-md text-gray-100 p-3 rounded-lg w-full 
                       focus:outline-none focus:shadow-outline"
+                    type="submit"
+                    value="Send"
                   >
                     Send Message
                   </button>
