1c6d09693e216733423fed9de871986293283291
asqarbekolimov <mr.coderx.pro@gmail.com>
2023-02-18 21:01:05 +0500

    HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import "./styles/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
